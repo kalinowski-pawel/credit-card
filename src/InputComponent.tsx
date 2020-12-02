823b78e3cf4008e0c9cbfcd8483702a120b70ea8
pablo <pkalinowski83@gmail.com>
2020-12-02 20:00:16 +0100

    The first commit

---
+++ src/InputComponent.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { ReactComponent } from '*.svg';
+
+interface PropsInterface {
+  maxLength?: number;
+  onChange: () => string;
+  value: string;
+}
+
+const InputComponent = (props: PropsInterface) => {
+  const [validation, setValidation] = React.useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = React.useState<string>('');
+
+  onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log('test', event);
+    props.onChange();
+  };
+
+  return (
+    <React.Fragment>
+      <input maxLength={props.maxLength} onChange={onChange} value={props.value} />
+      <div></div>
+    </React.Fragment>
+  );
+};
